518db8a8b7044a8f4995ea7f1fdd470ed2c6265a
Filip-Ericsson <filip.ericsson@elev.ga.ntig.se>
2020-02-20 15:03:30 +0100

    FirstPush
    Skapade lazerBeam och basSkeppet

---
+++ SlutprojektSpaceShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject lazerBeam;
+    
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Instantiate(lazerBeam);
+        }
+        
+    }
+}
